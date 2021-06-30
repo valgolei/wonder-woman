790243471609dc8ec1ed4bb5796d5313bec38b1d
valgolei <84638017+valgolei@users.noreply.github.com>
2021-06-30 12:26:58 +0200

    Mise à jour main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,37 @@
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairLarge, function (sprite, location) {
-    game.over(true)
+    info.startCountdown(120)
+    tiles.setTilemap(tilemap`niveau0`)
+    scene.setBackgroundColor(15)
+    tiles.placeOnRandomTile(mySprite, sprites.dungeon.stairWest)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprite.destroy()
+    otherSprite.destroy()
+    game.over(false)
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . 
+    . . . . . . 5 5 . . . . . . . 
+    . . . . . 5 5 5 5 . . . . . . 
+    . . . . 5 5 5 5 5 5 . . . . . 
+    . . . . f f f f f f . . . . . 
+    . . . . f 9 f f 9 f . . . . . 
+    . . . . f f f f f f . . . . . 
+    . . f e 5 5 5 5 5 5 e f . . . 
+    . . . . 5 5 5 5 5 5 . . . . . 
+    . . . . 5 5 5 5 5 5 . . . . . 
+    . . . . f f f f f f . . . . . 
+    . . . . . f . . f . . . . . . 
+    . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
+scene.setBackgroundColor(15)
+tiles.setTilemap(tilemap`niveau4`)
+tiles.placeOnRandomTile(mySprite, sprites.dungeon.stairWest)
+scene.cameraFollowSprite(mySprite)
+info.startCountdown(20)
